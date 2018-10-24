d54803d9d5bebe12b406f36bbf33945bc10d29b9
luckyluckycool <gonsor.y2@gmail.com>
2018-10-24 18:41:14 +0300

    gg

--- superapplolever/app/src/main/java/com/example/user/superapplolever/ResultActivity.java
+++ superapplolever/app/src/main/java/com/example/user/superapplolever/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.superapplolever;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,5 +16,13 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         text3.setText();
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(this, MainActivity);
+                startActivity(intent2);
+            }
+        });
+
     }
 }
