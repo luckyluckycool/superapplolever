37f155bfaa0b7cdc9679b9a1b09396678e057fad
luckyluckycool <gonsor.y2@gmail.com>
2018-10-24 18:05:15 +0300

    added

---
+++ superapplolever/app/src/main/java/com/example/user/superapplolever/HideClass.java
@@ -0,0 +1,19 @@
+package com.example.user.superapplolever;
+
+public class HideClass {
+    public double sum(double a, double b){
+        return a+b;
+    }
+
+    public double ext(double a, double b){
+        return a-b;
+    }
+
+    public double mult(double a, double b){
+        return a*b;
+    }
+
+    public double div(double a, double b){
+        return a/b;
+    }
+}
